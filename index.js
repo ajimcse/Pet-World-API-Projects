e283d969b350f5e3df4b7ecad40b23a32dab099f
ajimcse <mdajimsorkarcse40@gmail.com>
2026-09-19 00:00:03 +0600

    load data

---
+++ index.js
@@ -0,0 +1,58 @@
+const loadPets = () => {
+    fetch('https://openapi.programming-hero.com/api/peddy/pets')
+        .then(res => res.json())
+        .then(data => {
+            displayPets(data.pets);
+        })
+        .catch(error => console.log(error));
+};
+
+
+const displayPets = (pets) => {
+
+    const petContainer = document.getElementById('pet-container');
+
+    petContainer.innerHTML = "";
+
+    pets.forEach((pet) => {
+
+        const card = document.createElement('div');
+
+        card.innerHTML = `
+            <div class="card bg-base-100 shadow-xl">
+                <figure>
+                    <img 
+                        src="${pet.image}" 
+                        alt="${pet.pet_name}"
+                        class="w-full h-60 object-cover"
+                    />
+                </figure>
+
+                <div class="card-body">
+
+                    <h2 class="card-title">
+                        ${pet.pet_name}
+                    </h2>
+
+                    <p>Breed: ${pet.breed || "Not available"}</p>
+
+                    <p>Category: ${pet.category}</p>
+
+                    <p>Gender: ${pet.gender || "Not available"}</p>
+
+                    <p>Price: $${pet.price ?? "Not available"}</p>
+
+                    <button class="btn btn-primary">
+                        Details
+                    </button>
+
+                </div>
+            </div>
+        `;
+
+        petContainer.appendChild(card);
+    });
+};
+
+
+loadPets();
