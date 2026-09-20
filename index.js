5ddd18f1f4d30624b548e2570defc7957e79fc86
ajimcse <mdajimsorkarcse40@gmail.com>
2026-09-20 19:30:57 +0600

    category button

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+// banner button
+const loadButton = () => {
+    fetch('https://openapi.programming-hero.com/api/peddy/categories')
+        .then(res => res.json())
+        .then(data => displayButton(data.categories))
+        .catch(error => console.log(error));
+}
+const displayButton = (btns) => {
+    console.log(btns)
+    const buttonContainer= document.getElementById('banner-button')
+    btns.forEach((btn) => {
+        const button = document.createElement('button');
+        button.innerHTML = `
+            <img 
+                class="w-10 h-10 rounded-full"
+                src="${btn.category_icon}" 
+                alt=""
+            >
+
+            <span>${btn.category}</span>
+        `;
+
+        button.className = `
+            flex items-center gap-3
+            border p-3 rounded-lg
+        `;
+      buttonContainer.appendChild(button)
+
+    })
+}
+loadButton()
+
+
+
+// banner section
 const loadPets = () => {
     fetch('https://openapi.programming-hero.com/api/peddy/pets')
         .then(res => res.json())
